d4ee4feb82822a5a0edc8d5cf5b17629c1882263
Nathan Waterman <nathanjwuk@gmail.com>
2018-09-18 17:36:56 -0500

    Added roundwinner function. Started testing on roundwinner

--- Project_1_Rock_Paper_Scissors_99/js/game-logic.js
+++ Project_1_Rock_Paper_Scissors_99/js/game-logic.js
@@ -2,16 +2,16 @@
 let playerOneMoveOneType = undefined;
 let playerOneMoveTwoType = undefined;
 let playerOneMoveThreeType = undefined;
-let playerTwoMoveOneType = undefined;
-let playerTwoMoveTwoType = undefined;
-let playerTwoMoveThreeType = undefined;
-
 let playerOneMoveOneValue = undefined;
 let playerOneMoveTwoValue = undefined;
 let playerOneMoveThreeValue = undefined;
+
 let playerTwoMoveOneValue = undefined;
 let playerTwoMoveTwoValue = undefined;
 let playerTwoMoveThreeValue = undefined;
+let playerTwoMoveOneType = undefined;
+let playerTwoMoveTwoType = undefined;
+let playerTwoMoveThreeType = undefined;
 
 const setPlayerMoves = ((player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
     const testValue = ((value) => {
@@ -24,50 +24,145 @@ const setPlayerMoves = ((player, moveOneType, moveOneValue, moveTwoType, moveTwo
             return true;
         }
     });
+    const testSum = ((valueOne, valueTwo, valueThree) => {
+        const valueSum = valueOne + valueTwo + valueThree;
+        if (valueSum >= 1 && valueSum <= 99) {
+            return true;
+        }
+    });
+
     switch (player) {
     case 'Player One':
-        if (testValue(moveOneValue) & testValue(moveTwoValue) & testValue(moveThreeValue) & moveOneValue + moveTwoValue + moveThreeValue <= 99 
-            && moveOneValue + moveTwoValue + moveThreeValue < 1) {
-            if (testValue(moveOneValue)) {
-                playerOneMoveOneValue = moveOneValue;
-            }
-            if (testValue(moveTwoValue)) {
-                playerOneMoveTwoValue = moveTwoValue;
-            }
-            if (testValue(moveThreeValue)) {
-                playerOneMoveThreeValue = moveThreeValue;
-            }
-        }
-        if (testMove(playerOneMoveOneType) && testMove(playerOneMoveTwoType) && testMove(playerOneMoveThreeType)) {
+        if (testValue(moveOneValue) && testValue(moveTwoValue) && testValue(moveThreeValue) && testSum(moveOneValue, moveTwoValue, moveThreeValue)
+            && testMove(moveOneType) && testMove(moveTwoType) && testMove(moveThreeType)) {
+            // console.log('passed');
+            playerOneMoveOneValue = moveOneValue;
+            playerOneMoveTwoValue = moveTwoValue;
+            playerOneMoveThreeValue = moveThreeValue;
             playerOneMoveOneType = moveOneType;
             playerOneMoveTwoType = moveTwoType;
             playerOneMoveThreeType = moveThreeType;
         }
         break;
     case 'Player Two':
-        if (testValue(moveOneValue)) {
+        if (testValue(moveOneValue) && testValue(moveTwoValue) && testValue(moveThreeValue) && testSum(moveOneValue, moveTwoValue, moveThreeValue)
+        && testMove(moveOneType) && testMove(moveTwoType) && testMove(moveThreeType)) {
             playerTwoMoveOneValue = moveOneValue;
-        }
-        if (testValue(moveTwoValue)) {
             playerTwoMoveTwoValue = moveTwoValue;
-        }
-        if (testValue(moveThreeValue)) {
             playerTwoMoveThreeValue = moveThreeValue;
-        }
-        if (testMove(playerTwoMoveOneType)) {
             playerTwoMoveOneType = moveOneType;
-        }
-        if (testMove(playerTwoMoveTwoType)) {
             playerTwoMoveTwoType = moveTwoType;
-        }
-        if (testMove(playerTwoMoveThreeType)) {
             playerTwoMoveThreeType = moveThreeType;
         }
         break;
     }
 });
 
+const getRoundWinner = ((round) => {
+    // if (!playerOneMoveOneType || !playerOneMoveOneValue || !playerOneMoveTwoType || !playerOneMoveTwoValue || !playerOneMoveThreeType || !playerOneMoveThreeValue
+    //     || !playerTwoMoveOneType || !playerTwoMoveOneValue || !playerTwoMoveTwoType || !playerTwoMoveTwoValue || !playerTwoMoveThreeType || !playerTwoMoveThreeValue) {
+    //         console.log('fail');
+    //     return null;
+    // }
+    switch (round) {
+    case 1:
+        if (playerOneMoveOneType && playerTwoMoveOneType && playerOneMoveOneValue && playerTwoMoveOneValue && playerOneMoveOneType === playerTwoMoveOneType) {
+            if (playerOneMoveOneValue > playerTwoMoveOneValue) {
+                return 'Player One';
+            } else if (playerOneMoveOneValue < playerTwoMoveOneValue) {
+                return 'Player Two';
+            } else {
+                return 'Tie';
+            }
+        } else if (playerOneMoveOneType === 'rock') {
+            if (playerTwoMoveOneType === 'paper') {
+                return 'Player Two';
+            } else if (playerTwoMoveOneType === 'scissors') {
+                return 'Player One';
+            }
+        } else if (playerOneMoveOneType === 'paper') {
+            if (playerTwoMoveOneType === 'rock') {
+                return 'Player One';
+            } else if (playerTwoMoveOneType === 'scissors') {
+                return 'Player Two';
+            }
+        } else if (playerOneMoveOneType === 'scissors') {
+            if (playerTwoMoveOneType === 'paper') {
+                return 'Player One';
+            } else if (playerTwoMoveOneType === 'rock') {
+                return 'Player Two';
+            }
+        } else {
+            return null;
+        }
+        break;
+    case 2:
+        if (playerOneMoveTwoType && playerTwoMoveTwoType && playerOneMoveTwoType === playerTwoMoveTwoType) {
+            if (playerOneMoveTwoValue > playerTwoMoveTwoValue) {
+                return 'Player One';
+            } else if (playerOneMoveTwoValue < playerTwoMoveTwoValue) {
+                return 'Player Two';
+            } else {
+                return 'Tie';
+            }
+        } else if (playerOneMoveTwoType === 'rock') {
+            if (playerTwoMoveTwoType === 'paper') {
+                return 'Player Two';
+            } else if (playerTwoMoveTwoType === 'scissors') {
+                return 'Player One';
+            }
+        } else if (playerOneMoveTwoType === 'paper') {
+            if (playerTwoMoveTwoType === 'rock') {
+                return 'Player One';
+            } else if (playerTwoMoveTwoType === 'scissors') {
+                return 'Player Two';
+            }
+        } else if (playerOneMoveTwoType === 'scissors') {
+            if (playerTwoMoveTwoType === 'paper') {
+                return 'Player One';
+            } else if (playerTwoMoveTwoType === 'rock') {
+                return 'Player Two';
+            }
+        } else {
+            return null;
+        }
+        break;
+    case 3:
+        if (playerOneMoveThreeType && playerTwoMoveThreeType && playerOneMoveThreeType === playerTwoMoveThreeType) {
+            if (playerOneMoveThreeValue > playerTwoMoveThreeValue) {
+                return 'Player One';
+            } else if (playerOneMoveThreeValue < playerTwoMoveThreeValue) {
+                return 'Player Two';
+            } else {
+                return 'Tie';
+            }
+        } else if (playerOneMoveThreeType === 'rock') {
+            if (playerTwoMoveThreeType === 'paper') {
+                return 'Player Two';
+            } else if (playerTwoMoveThreeType === 'scissors') {
+                return 'Player One';
+            }
+        } else if (playerOneMoveThreeType === 'paper') {
+            if (playerTwoMoveThreeType === 'rock') {
+                return 'Player One';
+            } else if (playerTwoMoveThreeType === 'scissors') {
+                return 'Player Two';
+            }
+        } else if (playerOneMoveThreeType === 'scissors') {
+            if (playerTwoMoveThreeType === 'paper') {
+                return 'Player One';
+            } else if (playerTwoMoveThreeType === 'rock') {
+                return 'Player Two';
+            }
+        } else {
+            return null;
+        }
+        break;
+    default:
+        return null;
+    }
+});
 
-setPlayerMoves('Player One', 'rock', 11, 'paper', 33, 'scissors', 44);
-// console.log()
+// setPlayerMoves('Player One', undefined, 11, 'paper', 33, 'scissors', 44);
+
 
