a16a469099ecb6357223b272005bde8e7d849a4d
Nathan Waterman <nathanjwuk@gmail.com>
2018-09-24 15:24:01 -0500

    Finished presetHandler.js and all tests passed

--- Project_2_Beat_Mix/presetHandler.js
+++ Project_2_Beat_Mix/presetHandler.js
@@ -2,7 +2,21 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (request, index, newArray = []) => {
+    // console.log(request);
+    if (request !== "GET" && request !== "PUT") {
+        return [400];
+    }
+    if (index < 0 || index > 3 || presets[index] === []) {
+        return [404];
+    }
+    if (request === "PUT") {
+        presets[index] = newArray;
+    }
+    return [200, presets[index]];
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
+
+// console.log(presetHandler("GET", 3));
