705a23f56c90c3007e460aa8b948d291795344c9
Nathan Waterman <nathanjwuk@gmail.com>
2018-10-22 14:08:39 -0500

    Completed The Boss Machine getAllMinions and getMinionById testing

--- Project_4_Boss_Machine/server.js
+++ Project_4_Boss_Machine/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const db = require("./server/db");
+
 module.exports = app;
 
 /* Do not change the following line! It is required for testing and allowing
@@ -12,6 +14,25 @@ const PORT = process.env.PORT || 4001;
 
 
 // Add middware for parsing request bodies here:
+app.get("/api/minions", (req, res, next) => {
+    res.status(200).send(db.getAllFromDatabase("minions"));
+});
+
+app.get("/api/minions/:minionId", (req, res, next) => {
+    const minions = db.getAllFromDatabase("minions");
+    let index;
+    for (let i = 0; i < minions.length; i++) {
+        if (minions[i].id === req.params.minionId) {
+            index = i;
+            break;
+        }
+    }
+    if (index !== undefined) {
+        res.status(200).send(minions[index]);
+    } else {
+        res.status(404).send();
+    }
+});
 
 
 // Mount your existing apiRouter below at the '/api' path.
@@ -21,5 +42,8 @@ const apiRouter = require('./server/api');
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+    });
 }
+
