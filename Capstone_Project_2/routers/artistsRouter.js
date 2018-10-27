3e11b9fd0b3af18716359221aff619a893073653
Nathan <nathanjwtx@nathanjwtx.io>
2018-10-27 13:59:08 -0500

    Capstone Project 2 - POST artist tests passed

--- Capstone_Project_2/routers/artistsRouter.js
+++ Capstone_Project_2/routers/artistsRouter.js
@@ -5,10 +5,10 @@ const morgan = require("morgan");
 const db = new sqlite3.Database(process.env.TEST_DATABASE || "./database.sqlite");
 const app = express();
 const artistRouter = express.Router();
-// app.use(express.json());
-// app.use(express.urlencoded());
-// app.use(morgan("tiny"));
-// app.use(express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(morgan("tiny"));
+app.use(express.static("./public"));
 
 module.exports = artistRouter;
 
@@ -39,6 +39,40 @@ artistRouter.get("/", (req, res, next) => {
     }
 });
 
-// artistRouter.post("/", (req, res, next) => {
+let validateArtist = (req, res, next) => {
+    const artistData = req.body.artist;
+    console.log(artistData);
+    if (!artistData.name || !artistData.dateOfBirth || !artistData.biography) {
+        /* don't use return below so that the error is passed back to the .post router
+        the error then gets handed by function (err)
+        */
+        res.status(400).send(); 
+    }
+    next();
+};
 
-// });
+artistRouter.post("/", validateArtist, (req, res, next) => {
+    const artistData = req.body.artist;
+    db.run(`insert into Artist (name, date_Of_Birth, biography, is_Currently_Employed)
+    values ($name, $dob, $bio, $emp);`, {
+        $name: artistData.name,
+        $dob: artistData.dateOfBirth,
+        $bio: artistData.biography,
+        $emp: 1
+    }, function (err) {
+        if (err) {
+            // throw new Error(err);
+            return res.status(400).send(err);
+        } else {
+            let rowID = this.lastID;
+            db.get("select * from Artist where id = $id", {$id: rowID}, 
+                (err, row) => {
+                    if (err){
+                        res.sendStatus(400);
+                    } else {
+                        res.status(201).send({artist: row});
+                    }
+                });
+        }
+    });
+});
