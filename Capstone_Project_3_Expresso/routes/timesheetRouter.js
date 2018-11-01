f46d9bc4e58ce55d9cbb954784a9fb6b77837579
Nathan <nathanjwtx@nathanjwtx.io>
2018-10-31 21:02:54 -0500

    Capstone Project 3 - continuing work on timesheet route

--- Capstone_Project_3_Expresso/routes/timesheetRouter.js
+++ Capstone_Project_3_Expresso/routes/timesheetRouter.js
@@ -20,91 +20,93 @@ let getParams = (req, res, next) => {
     next();
 };
 
-let checkEmp = (id) => {
-    console.log(id);
-    db.get("select * from Employee where id = $id", {$id: id},
+let checkEmp = (req, res, next) => {
+    db.get("select * from Employee where id = $id", {$id: req.empID},
         (err, row) => {
             if (err || row === undefined) {
-                return false;
-            } else {
-                return true;
+                res.status(404).send();
+            // } else {
+            //     next();
             }
         });
+        next();
 };
 
-let checkTS = (id) => {
-    db.get("select * from Timesheet where id = $id", {$id: id} , 
-        (err, row) => {
-            if (err || row === undefined) {
-                return null;
-            }
-        });
-};
+// let checkTS = (id) => {
+//     db.get("select * from Timesheet where id = $id", {$id: id} , 
+//         (err, row) => {
+//             if (err || row === undefined) {
+//                 return null;
+//             }
+//         });
+// };
 
-let dataCheck = (req, res, next) => {
-    const tsData = req.body.timesheet;
-    if (!tsData.hours || !tsData.rate || !tsData.date) {
-        res.sendStatus(400);
-    }
-    next();
-};
+// let dataCheck = (req, res, next) => {
+//     const tsData = req.body.timesheet;
+//     if (!tsData.hours || !tsData.rate || !tsData.date) {
+//         res.sendStatus(400);
+//     }
+//     next();
+// };
+
+https://stackoverflow.com/questions/10695629/what-is-the-parameter-next-used-for-in-express?noredirect=1&lq=1
 
 // get all timesheets
-timesheetRouter.get("/", getParams, (req, res, next) => {
-    console.log(checkEmp(req.empID));
-    if (req.empID && checkEmp(req.empID)) {
+timesheetRouter.get("/", getParams, checkEmp, (req, res, next) => {
+    if (req.empID) {
         db.all("select * from Timesheet where employee_id = $id", {$id: req.empID},
             (err, rows) => {
                 if (err) {
-                    throw new Error(err);
+                    next(err);
+                } else {
+                    return res.status(200).send({timesheets: rows});
                 }
-                return res.status(200).send({timesheets: rows});
             });
     } else {
         return res.status(404).send("Nothing to see here");
     }
 });
 
-// create new timesheet
-timesheetRouter.post("/", getParams, (req, res, next) => {
-    const tsData = req.body.timesheet;
-    console.log("ID", req.empID);
-    db.run(`insert into Timesheet (hours, rate, date, employee_id) 
-        values ($hours, $rate, $date, $emp);`, {
-        $hours: tsData.hours,
-        $rate: tsData.rate,
-        $date: tsData.date,
-        $emp: req.empID
-    }, function(err) {
-        if (err) {
-            return res.status(404).send(err);
-        } else {
-            db.get("select * from Timesheet where id = $id", {$id: this.lastID},
-                (err, row) => {
-                    return res.status(201).send({timesheet: row});
-                });
-        }
-    });
-});
+// // create new timesheet
+// timesheetRouter.post("/", getParams, (req, res, next) => {
+//     const tsData = req.body.timesheet;
+//     console.log("ID", req.empID);
+//     db.run(`insert into Timesheet (hours, rate, date, employee_id) 
+//         values ($hours, $rate, $date, $emp);`, {
+//         $hours: tsData.hours,
+//         $rate: tsData.rate,
+//         $date: tsData.date,
+//         $emp: req.empID
+//     }, function(err) {
+//         if (err) {
+//             return res.status(404).send(err);
+//         } else {
+//             db.get("select * from Timesheet where id = $id", {$id: this.lastID},
+//                 (err, row) => {
+//                     return res.status(201).send({timesheet: row});
+//                 });
+//         }
+//     });
+// });
 
-// edit existing timesheet
-timesheetRouter.put("/", getParams, checkEmp, checkTS, dataCheck, (req, res, next) => {
-    const tsData = req.body.timesheet;
-    console.log(tsData);
-    db.run(`update Timesheet set hours = $hours, rate = $rate, date = $rate 
-    where id = $id`, {
-        $hours: tsData.hours,
-        $rate: tsData.rate,
-        $date: tsData.date,
-        $id: req.tsID
-    }, function(err) {
-        if (err) {
-            return res.status(404).send(err);
-        }
-        db.get("select * from Timesheet where id = $id", {$id: this.lastID},
-            (err, row) => {
-                return res.status(201).send({timesheet: row});
-            });
-    });
-});
+// // edit existing timesheet
+// timesheetRouter.put("/", getParams, checkEmp, checkTS, dataCheck, (req, res, next) => {
+//     const tsData = req.body.timesheet;
+//     console.log(tsData);
+//     db.run(`update Timesheet set hours = $hours, rate = $rate, date = $rate 
+//     where id = $id`, {
+//         $hours: tsData.hours,
+//         $rate: tsData.rate,
+//         $date: tsData.date,
+//         $id: req.tsID
+//     }, function(err) {
+//         if (err) {
+//             return res.status(404).send(err);
+//         }
+//         db.get("select * from Timesheet where id = $id", {$id: this.lastID},
+//             (err, row) => {
+//                 return res.status(201).send({timesheet: row});
+//             });
+//     });
+// });
 
